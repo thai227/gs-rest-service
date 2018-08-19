4c1f5d4645a3a9aab2e99a2c2aa04767c747b8fb
thai <nhthai2207@gmail.com>
2018-08-19 21:32:50 +0700

    refactor structure

--- src/main/java/com/thai/application/api/GreetingAPI.java
+++ src/main/java/com/thai/application/api/GreetingAPI.java
@@ -1,17 +1,19 @@
-package hello;
+package com.thai.application.api;
 
-import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.thai.domain.greeting.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
-public class GreetingController {
+public class GreetingAPI {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
